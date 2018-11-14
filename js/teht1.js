12b958a5c3ff5fc2d556c42c0e6cbc09a7428899
kimahi <44357279+kimahi@users.noreply.github.com>
2018-11-14 17:24:40 +0200

    Update teht1.js

--- js/teht1.js
+++ js/teht1.js
@@ -1,5 +1,5 @@
 document.getElementsByTagName("td")[0].innerHTML = "<td>Kim</td>";
-document.getElementsByTagName("td")[1].innerHTML = "<td>Björklöf</td>";
+document.getElementsByTagName("td")[1].innerHTML = "<td>B</td>";
 document.getElementsByTagName("td")[3].innerHTML = "<td>Umpikuja 1 01620, VATNAA</td>";
 
 tr1 = document.getElementsByTagName("tr")[0];
